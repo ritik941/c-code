9ee77ac8891bf41b4973d614f4ccb08db1a3699b
Ritik Sharma <ritiksharma1205@gmail.com>
2024-03-23 08:40:53 +0530

    c++ if-else program

---
+++ c+.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int saving;
+cin>>saving;
+
+if(saving > 5000) {
+  cout<<"neha\n";
+
+}else if(saving=2000) {
+  cout<<"friends\n";
+} else {
+  cout<<"rashmi\n";
+}
+
+    return 0;
+}
