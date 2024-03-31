9945010d7e4da5c134786b08e937c4787301e9ca
Ritik Sharma <ritiksharma1205@gmail.com>
2024-03-31 08:16:16 +0530

    switch case c++

---
+++ switch.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char button;
+    cout<<"input a character";
+    cin>>button;
+
+    if (button=='a') {
+        cout<<"hello"<<endl;
+
+    }
+    else if (button == 'b') {
+        cout<<"namaste"<<endl;
+
+
+    }
+    else if (button=='c') {
+         cout<<"hola"<<endl;
+
+    }
+    else if (button =='d')  {
+        cout<<"hallo"<<endl;
+
+
+    }
+ return 0;   
+}
